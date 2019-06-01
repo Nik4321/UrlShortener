32c7b1644eacd72998b93e8a4bdfce07442dd0ba
Nik4321 <nikola4421@gmail.com>
2019-06-01 20:28:07 +0300

    Adding BaseEntity class to data layer

---
+++ UrlShortener.Data/Models/BaseEntity{T}.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UrlShortener.Data.Models.Interfaces;
+
+namespace UrlShortener.Data.Models
+{
+    public class BaseEntity<TKey> : IAudit
+    {
+        public TKey Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public string ModifiedBy { get; set; }
+    }
+}
---
+++ UrlShortener.Data/Models/Interfaces/IAudit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace UrlShortener.Data.Models.Interfaces
+{
+    public interface IAudit
+    {
+        DateTime CreatedOn { get; set; }
+
+        string CreatedBy { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+
+        string ModifiedBy { get; set; }
+    }
+}
