e5f2754f45bd6fdfcd8c9c0c8faaa23970066cc4
Nikola Ivanov <nikola4421@gmail.com>
2019-09-11 00:43:33 +0300

    Adding url service test

--- UrlShortener.Client/src/app/services/url.service.spec.ts
+++ UrlShortener.Client/src/app/services/url.service.spec.ts
@@ -1,12 +1,50 @@
 import { TestBed } from '@angular/core/testing';
 
 import { UrlService } from './url.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { LongUrlModel } from '../models/input/long-url.model';
+import { environment } from 'src/environments/environment';
 
 describe('UrlService', () => {
-	beforeEach(() => TestBed.configureTestingModule({}));
+	let service: UrlService;
+	let httpMock: HttpTestingController;
+
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			imports: [HttpClientTestingModule],
+			providers: [UrlService]
+		});
+
+		service = TestBed.get(UrlService);
+		httpMock = TestBed.get(HttpTestingController);
+	});
+
+	afterEach(() => {
+		httpMock.verify();
+	});
 
 	it('should be created', () => {
-		const service: UrlService = TestBed.get(UrlService);
-		expect(service).toBeTruthy();
+		expect(service).toBeDefined();
+	});
+
+	it('should shorten url', () => {
+		const googleUrl = 'http://google.com';
+		const longUrl = new LongUrlModel(googleUrl);
+		service.shortenUrl(longUrl).subscribe((response) => {
+			expect(response.longUrl).toBe(googleUrl);
+			expect(response.shortUrl).not.toBeNull();
+			expect(response.shortUrl).not.toBeNull();
+		});
+
+		const req = httpMock.expectOne(
+			environment.apiUrl,
+			'post to shorten url endpoint'
+		);
+		expect(req.request.method).toBe('POST');
+
+		req.flush({
+			longUrl: googleUrl,
+			shortUrl: 'localhost:5000/9ht74kb5',
+		});
 	});
 });
