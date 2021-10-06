79bc9643e9b2fb71756de0f60e031476b0f2c2fb
Nikola Ivanov <nikola4421@gmail.com>
2021-10-06 11:53:27 +0300

    Adding UrlHelpers unit tests

--- src/UrlShortener.Infrastructure/Helpers/UrlHelpers.cs
+++ src/UrlShortener.Infrastructure/Helpers/UrlHelpers.cs
@@ -14,6 +14,8 @@ namespace UrlShortener.Infrastructure.Helpers
         /// <returns>The unix time converted to <see cref="DateTime"/></returns>
         public static DateTime ConvertUnixTimeToDateTime(long unixTimeStamp)
         {
+            AssertArgument.NotNull(unixTimeStamp, nameof(unixTimeStamp));
+
             var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             var result = dateTime.AddSeconds(unixTimeStamp);
             return result;
---
+++ tests/UrlShortener.Infrastructure.Tests/Helpers/UrlHelpersTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UrlShortener.Infrastructure.Helpers;
+using Xunit;
+
+namespace UrlShortener.Infrastructure.Tests.Helpers
+{
+    public class UrlHelpersTests
+    {
+        [Fact]
+        public void ConvertUnixTimeToDateTime_ShouldConvertTimeCorrectly()
+        {
+            // 03/08/2020 @ 6:23:30pm - 1583691810
+            var expected = new DateTime(2020, 03, 08, 18, 23, 30, DateTimeKind.Utc);
+            var unixTime = 1583691810;
+
+            var actual = UrlHelpers.ConvertUnixTimeToDateTime(unixTime);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
