dee0fa34e2b77bff97cd2a08eb779860ed12ea7b
Nikola Ivanov <nikola4421@gmail.com>
2022-02-09 12:42:14 +0200

    Adding mapper profiles unit tests

---
+++ tests/UrlShortener.Infrastructure.Tests/MapperProfiles/ProfileValidityTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UrlShortener.Data.MapperProfiles;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace UrlShortener.Infrastructure.Tests.MapperProfiles
+{
+    public class ProfileValidityTests
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public ProfileValidityTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public void AllProfilesShouldHaveValidConfiguration()
+        {
+            var sut = new MapperConfiguration(x => x.AddMaps(typeof(UrlProfile).Assembly));
+
+            var maps = sut.GetAllTypeMaps();
+
+            foreach (var map in maps)
+            {
+                foreach (var prop in map.PropertyMaps)
+                {
+                    if (prop.SourceType != prop.DestinationType)
+                    {
+                        try
+                        {
+                            this.testOutputHelper.WriteLine(
+                                $"Warning: {map.DestinationType} > {map.SourceType.Name} - {prop.SourceMember.Name} does not match {prop.DestinationMember.Name}");
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            sut.AssertConfigurationIsValid();
+        }
+    }
+}
